0463f97e1af5ab2eb1a07c6815a6b5468224fce2
longliveCANC <1357254473@qq.com>
2024-05-11 21:12:02 +0800

    补全ex1-1

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
@@ -43,6 +43,10 @@ public class Ch3Ex1Activity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // TODO ex1-2: 这里应该调用哪个函数呢
+                float animationProgress = progress / 100.0f;
+
+                // 设置 LottieAnimationView 的进度
+                animationView.setProgress(animationProgress);
                 // 提示1：可以参考 https://airbnb.io/lottie/#/android?id=custom-animators
                 // 提示2：SeekBar 的文档可以把鼠标放在 OnProgressChanged 中间，并点击 F1 查看，
                 // 或者到官网查询 https://developer.android.google.cn/reference/android/widget/SeekBar.OnSeekBarChangeListener.html#onProgressChanged(android.widget.SeekBar,%20int,%20boolean
